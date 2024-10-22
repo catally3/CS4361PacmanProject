4ad2a15b61be49b76df8b43d808e36a43f1dcde8
b3tch1 <167640503+b3tch1@users.noreply.github.com>
2024-10-22 10:00:23 -0500

    Added rough structure for PacMan object

--- PacmanProject/Assets/PacMan.cs
+++ PacmanProject/Assets/PacMan.cs
@@ -4,47 +4,73 @@ using UnityEngine;
 
 public class PacMan : MonoBehaviour
 {
-    public int remainingLives;
-    
-    public bool isPowered;
+    const int totalPellets;             // Fill this in when board is finished
 
-    static void MoveToStart(){
+
+    public int remainingLives;          // If remainingLives is 0, game over
+    public int remainingPellets;        // When remainingPellets is 0, game over
+    public bool isPoweredUp;            // If isPoweredUp is true PacMan can eat ghosts
+    public Vector3 currentDirection;
+    
+
+    private static void MoveToStart(){
+        // Set these values to starting position, when board is finished
         const int xStartPosition;
         const int yStartPosition;
         const int zStartPosition;
         
         transform.position = new Vector3(xStartPosition, yStartPosition, zStartPosition);
     }
-
-    static void SetDirection(int zDirection){
-        transform.position = new Vector3(transform.position.x, transform.position.y, zDirection);
+    
+    private static void SetDirection(int zDirection){
+        currentDirection = new Vector3(transform.position.x, transform.position.y, zDirection);
+        transform.position = currentDirection;
     }
 
     // Initialize PacMan on game start
-    static void InitializePacMan(){
+    private static void InitializePacMan(){
         remainingLives = 3;
+        remainingPellets = totalPellets;
         isPowered = false;
         MoveToStart();
         SetDirection(0);
     }
 
-    // Reset PacMan on death    
-    static void ResetPacMan(){
+    // Call when PacMan collides with a ghost and isPoweredUp is false 
+    public void ResetPacMan(){
         remainingLives--;
         isPowered = false;
         MoveToStart();
         SetDirection(0);
     }
     
+    // Call when PacMan collides with a wall
+    public void ReverseDirection(){
+
+    }
+
+    // Call when PacMan collides with a pellet
+    public void CollectPellet(){
+        remainingPellets--;
+    }
+
+    // Call when PacMan collides with a power pellet
+    public void PowerUp(){
+        isPoweredUp = true;
+        // start timer
+
+        // finish timer
+        isPoweredUp = false;
+    }
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        InitializePacMan();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        // Update currentDirection when a movement key is pressed
+        // Move forward continuously
     }
 }
