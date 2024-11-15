3463606d30bc5ab7954fb725310c4bb3f1714022
catally3 <68207926+catally3@users.noreply.github.com>
2024-11-14 18:35:08 -0600

    Moved pellet code
    moved pellet and power pellet code to the scripts folder

---
+++ PacmanProject/Assets/Scripts/Pellet.cs
@@ -0,0 +1,20 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pellet : MonoBehaviour
+{
+    public int scoreValue = 10; // Score value for each pellet
+
+    private void OnCollisionEnter(Collision collison)
+    {
+        if (collison.gameObject.CompareTag("Pacman"))
+        {
+            // Add score to the game manager
+            GameManager.instance.AddScore(scoreValue);
+            // Destroy the pellet
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ PacmanProject/Assets/Scripts/PowerPellet.cs
@@ -0,0 +1,24 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerPellet : MonoBehaviour
+{
+    public int scoreValue = 50; // Higher score value for each power pellet
+    public float powerDuration = 10f; // Duration of power mode in seconds
+
+    private void OnCollisionEnter(Collision collison)
+    {
+        if (collison.gameObject.CompareTag("Pacman")) { 
+            // Add score to the game manager
+            GameManager.instance.AddScore(scoreValue);
+
+            // Activate power mode for Pac-Man, allowing him to eat ghosts
+            GameManager.instance.ActivatePowerMode(powerDuration);
+
+            // Destroy the power pellet
+            Destroy(gameObject);
+        }
+    }
+}
